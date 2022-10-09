e80bff4042ebafad768c9157686a30c3e74689f9
shamirulsaimon <shamirulrhaman@gmail.com>
2022-10-09 15:41:15 +0600

    even and odd

---
+++ DAY -3/Odd or Even/main.py
@@ -0,0 +1,8 @@
+print("Enter a number you want to check!")
+
+n = int(input("Enter The  number: "))
+
+if(n%2==0):          # Modulo Oerator " % "
+    print("The number is Even")
+else:
+    print("The number is ODD")
