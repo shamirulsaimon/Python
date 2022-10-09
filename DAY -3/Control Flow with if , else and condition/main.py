02ebe1af1488552b6adb276cf05819769be9f25a
shamirulsaimon <shamirulrhaman@gmail.com>
2022-10-09 13:00:33 +0600

    Basic Python

---
+++ DAY -3/Control Flow with if , else and condition/main.py
@@ -0,0 +1,25 @@
+print("Welcome to the rollercoaster!")
+print("Please Enter Your height")
+height = int(input("Your height in CM : "))
+if height >= 120:
+    print("Yon can ride the rollarcoaster")
+else:
+    print("Sorry!You have to grow taller")
+    
+    
+if height == 120:
+    print("Yon can ride the rollarcoaster")
+else:
+    print("Sorry!You have to grow taller")
+    
+if height != 120:
+    
+    print("Sorry!You have to grow taller")
+else:
+   print("Yon can ride the rollarcoaster")
+    
+    
+if height <= 120:
+   print("Sorry!You have to grow taller")
+else:
+    print("Yon can ride the rollarcoaster")
