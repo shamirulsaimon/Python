5dde937029edf3cbb69a798edaf7bf3c94c394ea
shamirulsaimon <shamirulrhaman@gmail.com>
2022-10-17 22:14:23 +0600

    List And Add to list

---
+++ DAY -4/The Offset And Appending/main.py
@@ -0,0 +1,15 @@
+
+
+fruits = ["Apple" , "Orange" , "lemon"] # list helps to store many data
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+
+# input some data in list
+
+fruits.append("Water Melon") 
+
+fruits[2] = "Jack Fruit"
+
+print(fruits)
