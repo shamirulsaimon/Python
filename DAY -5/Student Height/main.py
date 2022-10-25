6a7c23dc7c39eef838b5732a5fbf6667f989266b
shamirulsaimon <shamirulrhaman@gmail.com>
2022-10-25 21:53:19 +0600

    Average Height Using For loop

--- DAY -5/Student Height/main.py
+++ DAY -5/Student Height/main.py
@@ -1,4 +1,4 @@
-student_heights = input("Input a list of student heights ").split()
+student_heights = input("Input a list of student heights: ").split()
 
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
@@ -7,15 +7,14 @@ total_height = 0
 for height in student_heights:
     total_height += height
     
-print(total_height)
+print(f"Total Height Of the Student is {total_height}")
 
 number_of_student = 0
 for student in student_heights:
     number_of_student +=1
-    
-print(number_of_student)
 
+print(f"Number Of Students Heights are {number_of_student}")
 
 average = "{:.2f}".format(total_height/number_of_student)
 
-print(average)
+print(f"The Average Height Of The Students are {average}")
