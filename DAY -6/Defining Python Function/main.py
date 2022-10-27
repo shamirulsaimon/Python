5803924ac6528058ab7cfb88aa398b83c4bf54f3
shamirulsaimon <shamirulrhaman@gmail.com>
2022-10-28 00:14:54 +0600

    Define A function in python

---
+++ DAY -6/Defining Python Function/main.py
@@ -0,0 +1,25 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+letter = int(input("Enter how many letters you want in your password: "))
+number = int(input("How many Number you want in your password : "))
+symbol = int(input("How many symbols you want in your password : "))
+
+def password_generator(): # define a function
+    password=[]
+    for char in range(0,letter+1):
+        password.append(random.choice(letters))
+    for char in range(0,number+1):
+        password.append(random.choice(numbers))
+    for char in range(0,symbol+1):
+        password.append(random.choice(symbols))
+        
+    random.shuffle(password)
+    passwo = ""
+    for char in password:
+        passwo +=char
+    print(passwo)
+password_generator() # call a funtion
