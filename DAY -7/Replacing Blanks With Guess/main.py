c271a869191a772ff727b4d8611feec25b70d238
shamirulsaimon <shamirulrhaman@gmail.com>
2022-11-13 20:23:28 +0600

    Repslceing Black with word using While loop

--- DAY -7/Replacing Blanks With Guess/main.py
+++ DAY -7/Replacing Blanks With Guess/main.py
@@ -1,24 +1,34 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
 
 print(f'Pssst, the solution is {chosen_word}.')
 
-#Step 1: - Create an empty List called display.
+#Create blanks
 display = []
-word_length = len(chosen_word)
 for _ in range(word_length):
     display += "_"
 
-guess = input("Guess a letter: ").lower()
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+end_of_game = False
 
-#Step 2: - Loop through each position in the chosen_word;
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-print(display)
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
 
+    print(display)
+
+    #Check if there are no more "_" left in 'display'. Then all letters have been guessed.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
 
 
