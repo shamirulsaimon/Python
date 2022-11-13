ca8776da9789759d6c55b2e20dff83303f5aaa13
shamirulsaimon <shamirulrhaman@gmail.com>
2022-11-13 22:09:24 +0600

    One commit is added

--- DAY -7/Replacing Blanks With Guess lives/main.py
+++ DAY -7/Replacing Blanks With Guess lives/main.py
@@ -96,7 +96,7 @@ while not end_of_game:
             print("You lose.")
 
     #Join all the elements in the list and turn it into a String.
-    print(f"{' '.join(display)}")
+    print(f"{' '.join(display)}") #takes all items in an iterable and joins them into one string
 
     #Check if user has got all letters.
     if "_" not in display:
