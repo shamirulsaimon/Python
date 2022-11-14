74b161c096dfa5fe64ae0f4a5afff889e8b5609b
shamirulsaimon <shamirulrhaman@gmail.com>
2022-11-14 21:05:15 +0600

    Position and keyword with Function

--- DAY -8/Function With input/main.py
+++ DAY -8/Function With input/main.py
@@ -11,3 +11,11 @@ def greet_with_name(name):
     print(f"Isn't the Weather nice Today ? {name} ")
     
 greet_with_name("Saimon")
+
+
+def greet_with_name(name,location):
+    print(f"Hello {name} ")
+    print(f"How are you ? {name} ")
+    print(f"Isn't the Weather nice Today ? {name} ")
+    print(f"How is it like to be in  {location} ?")
+greet_with_name(name = "Shamirul" , location = "London")
